f319fa473989340f4204657509ae4218e0883a09
Imani Oliver <isaac@ihardy.co>
2017-08-24 11:04:00 -0400

    edit is partially working

--- routes/index.js
+++ routes/index.js
@@ -39,9 +39,9 @@ router.post('/', function (req, res){
     res.redirect('/');
 })
 
-router.post('/delete', function(req, res){
+router.post('/delete/', function(req, res){
     let name = req.body.name;
-    console.log(name);
+    // console.log(name);
 
       Pokemon.deleteOne({name:name})
       .then(function(data) {
@@ -54,51 +54,55 @@ router.post('/delete', function(req, res){
 });
 
 router.get('/edit/:name', function(req, res){
-    res.render('edit');
+
+    let name = req.params.name;
+    console.log(name);
+    Pokemon.find({name: name})
+    // console.log({name:name})
+    .then(function(pokemon) {
+        // console.log('stuff');
+        console.log(pokemon);
+        res.render("edit", {pokemonCatchInfo: pokemon});
+    })
 });
 
 router.post('/edit/:name', function(req, res){
 
+    let newPokemon = {
+        name: req.body.name,
+        gender: req.body.gender,
+        type: req.body.type,
+        candy: req.body.candy,
+        attacks: req.body.attacks,
+        catchInfo: {
+            city: req.body.city,
+            state: req.body.state,
+        },
+        hp: req.body.hp,
+        cp: req.body.cp,
+    };
 
-    // Pokemon.updateOne({
-    //     name: req.body.name,
-    //     gender: req.body.gender,
-    //     type: req.body.type,
-    //     candy: req.body.candy,
-    //     attacks: req.body.attacks,
-    //     catchInfo: {
-    //         city: req.body.city,
-    //         state: req.body.state,
-    //         },
-    //     hp: req.body.hp,
-    //     cp: req.body.cp,
-    //     }
-    // , {$push. {
-    //     name: req.body.name,
-    //     gender: req.body.gender,
-    //     type: req.body.type,
-    //     candy: req.body.candy,
-    //     attacks: req.body.attacks,
-    //     catchInfo: {
-    //         city: req.body.city,
-    //         state: req.body.state,
-    //         },
-    //     hp: req.body.hp,
-    //     cp: req.body.cp,}
-    //     })
+let name = req.params.name
 
+    Pokemon.update({name:name}, {
+        name: req.body.name,
+        gender: req.body.gender,
+        type: req.body.type,
+        candy: req.body.candy,
+        attacks: req.body.attacks,
+        catchInfo: {
+            city: req.body.city,
+            state: req.body.state,
+        },
+        hp: req.body.hp,
+        cp: req.body.cp,
+    }).then(function(data) {
+        res.redirect('/');
+    })
 
     //
     //
-    // .then(function(data){
-    //
-    // })
-    // .catch(function(err){
-    //
-    // });
 
-
-    res.redirect('/');
 });
 
 
---
+++ routes/tried.js
@@ -0,0 +1,36 @@
+// Pokemon.updateOne({
+//     name: req.body.name,
+//     gender: req.body.gender,
+//     type: req.body.type,
+//     candy: req.body.candy,
+//     attacks: req.body.attacks,
+//     catchInfo: {
+//         city: req.body.city,
+//         state: req.body.state,
+//         },
+//     hp: req.body.hp,
+//     cp: req.body.cp,
+//     }
+// , {$push. {
+//     name: req.body.name,
+//     gender: req.body.gender,
+//     type: req.body.type,
+//     candy: req.body.candy,
+//     attacks: req.body.attacks,
+//     catchInfo: {
+//         city: req.body.city,
+//         state: req.body.state,
+//         },
+//     hp: req.body.hp,
+//     cp: req.body.cp,}
+//     })
+
+
+//
+//
+// .then(function(data){
+//
+// })
+// .catch(function(err){
+//
+// });
